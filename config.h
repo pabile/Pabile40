2def4df1214a89fa1a80c787a6b99828164843c3
pabile <1162412+pabile@users.noreply.github.com>
2019-11-12 22:32:17 +0800

    to reflect pcb v0.01
    original handwired config:
    #define MATRIX_ROW_PINS { F4, F5, F6, F7 }
    #define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5, B1, B3, B2, B6 }

--- config.h
+++ config.h
@@ -15,8 +15,8 @@
 #define MATRIX_COLS 10
 
 /* pin-out */
-#define MATRIX_ROW_PINS { F4, F5, F6, F7 }
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS { F6, B3, B2, B6 }
+#define MATRIX_COL_PINS { F4, F5, B5, B4, E6, D7, C6, D4, D0, D1 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
