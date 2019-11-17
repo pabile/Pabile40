be8a9854c333c5c5ffa57d5a224e16c99f1e02cd
pabile <1162412+pabile@users.noreply.github.com>
2019-11-17 10:48:45 +0800

    Update config.h
    too quick to shift for layer keys when using tapping_term. adjusting speed may also work but used retro_tapping for now. seems to work better.

--- config.h
+++ config.h
@@ -14,7 +14,8 @@
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 10
 
-/* pin-out */
+/* pin-out
+for PCB ver0.01 452019 */
 #define MATRIX_ROW_PINS { F6, B3, B2, B6 }
 #define MATRIX_COL_PINS { F4, F5, B5, B4, E6, D7, C6, D4, D0, D1 }
 #define UNUSED_PINS
@@ -22,5 +23,7 @@
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-/*#define TAPPING_TERM 200*/
+/*
+#define TAPPING_TERM 200
+*/
 #define RETRO_TAPPING
